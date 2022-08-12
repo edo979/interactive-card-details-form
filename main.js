e0f461a42bc29e41cf888d94e1a2a9618f4700cf
edo979 <prezentabilan@gmail.com>
2022-08-12 15:18:47 +0200

    validate form for empty input

--- main.js
+++ main.js
@@ -6,5 +6,19 @@ formEl.addEventListener('input', (e) => {
   const inputName = e.target.dataset.cardInfo,
     targetFieldEl = document.querySelector(`[data-card-preview='${inputName}']`)
 
-  targetFieldEl.textContent = e.target.value
+  if (isUserInputValid(e.target)) {
+    targetFieldEl.textContent = e.target.value
+  } else {
+    targetFieldEl.textContent = 'Input error!'
+  }
 })
+
+function isUserInputValid(inputEl) {
+  if (inputEl.value == '') {
+    inputEl.parentElement.classList.add('error')
+    return false
+  } else {
+    inputEl.parentElement.classList.remove('error')
+    return true
+  }
+}
