b4ae2c381ba1d2584370f9266f93873f4e82806e
edo979 <prezentabilan@gmail.com>
2022-08-12 08:46:54 +0200

    finish onchange handler

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import './scss/style.scss'
+
+const formEl = document.getElementById('form')
+
+formEl.addEventListener('input', (e) => {
+  const inputName = e.target.dataset.cardInfo,
+    targetFieldEl = document.querySelector(`[data-card-preview='${inputName}']`)
+
+  targetFieldEl.textContent = e.target.value
+})
