dc0bb93a553667ab9149bbd08bf33a955cebba34
edo979 <prezentabilan@gmail.com>
2022-08-12 17:24:12 +0200

    finish validate input format

--- main.js
+++ main.js
@@ -13,12 +13,40 @@ formEl.addEventListener('input', (e) => {
   }
 })
 
+/** @param {Node} inputEl */
 function isUserInputValid(inputEl) {
+  // prettier-ignore
+  const reg = /^[\s\d]+$/
+  let isInputValid = true,
+    errorMsg = "Can't be blank"
+
   if (inputEl.value == '') {
+    isInputValid = false
+  } else if (
+    // skip validate cardholder name
+    inputEl.dataset.cardInfo != 'cardholder' &&
+    !reg.test(inputEl.value)
+  ) {
+    isInputValid = false
+    errorMsg = 'Wrong format, numbers only'
+  } else {
+    isInputValid = true
+  }
+
+  if (!isInputValid) {
     inputEl.parentElement.classList.add('error')
-    return false
+    showErrorMessage(inputEl, errorMsg)
   } else {
     inputEl.parentElement.classList.remove('error')
-    return true
   }
+
+  return isInputValid
+}
+
+/**
+ * @param {Node} inputEl
+ * @param {String} errorMsg
+ */
+function showErrorMessage(inputEl, errorMsg) {
+  inputEl.parentElement.querySelector('.error-msg').textContent = errorMsg
 }
